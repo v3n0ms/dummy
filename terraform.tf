5ed120ebfab1ded35b54948f6507dfc2350237bb
Reddy-Somith <reddysomith.r@thoughtworks.com>
2022-10-14 13:01:25 +0530

    gg 1

--- terraform.tf
+++ terraform.tf
@@ -3,3 +3,12 @@ provider "google" {
   project     = "dummy-project-365407"
   region      = "us-east4"
 }
+
+terraform {
+  backend "gcs" {
+    bucket      = "demo-bucket-dummy-00"
+    path        = "dev/terraform.tfstate"
+    project     = "dummy-project-365407"
+    credentials = file("gcs.json")
+  }
+}
