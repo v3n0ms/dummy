ea000077273abca7b1b0bad161b4937e4fb2182c
Reddy-Somith <reddysomith.r@thoughtworks.com>
2022-11-08 12:59:25 +0530

    np

--- main.tf
+++ main.tf
@@ -1,41 +1,21 @@
-resource "google_compute_instance" "default" {
-  name         = "retail-pos-helloworld"
-  machine_type = "f1-micro"
-  zone         = "us-east4-b"
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-  network_interface {
-    network = google_compute_network.default.name
-
-    access_config {
-
-    }
-  }
-
-  metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y &&  echo 'hello world' | sudo tee /var/www/html/index.html"
-  tags                    = ["http-server"]
+resource “google_sql_database_instance” “master” {
+name = "instance_name"
+database_version = "MYSQL_8"
+region = "us-east4"
+settings {
+tier = "db-f1-micro"
 }
-
-resource "google_compute_firewall" "http-server" {
-
-  name    = "default-allow-http-terraform"
-  network = google_compute_network.default.name
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80"]
-  }
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["http-server"]
 }
-
-
-output "ip" {
-
-  value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
-
+resource "google_sql_database" “database” {
+name = "pos"
+instance = "${google_sql_database_instance.master.name}"
+charset = "utf8"
+collation = "utf8_general_ci"
+}
+resource "google_sql_user" "users" {
+name = "root"
+instance = "${google_sql_database_instance.master.name}"
+host = "%"
+password = "Sup3r$ecretP@ss"
 }
--- network.tf
+++
@@ -1,9 +0,0 @@
-resource "google_compute_network" "default" {
-  name = "default"
-}
-resource "google_compute_subnetwork" "public-subnetwork" {
-  name          = "terraform-subnetwork"
-  ip_cidr_range = "10.2.0.0/16"
-  region        = "us-east4"
-  network       = google_compute_network.default.name
-}
