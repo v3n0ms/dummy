823067a3c3781e410be6a23e9dc8b0b4bb18b0cc
Reddy-Somith <reddysomith.r@thoughtworks.com>
2022-11-08 15:16:09 +0530

    np 6

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ database_version = "MYSQL_8_0"
 region = "us-east4"
 settings {
 tier = "db-f1-micro"
+ip_configuration {
+      ipv4_enabled    = true
+      authorized_networks{
+        value = "0.0.0.0/0"
+    }
+}
 }
 }
 resource "google_sql_database" "database" {
