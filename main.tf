2f061127ebc131b1734e098d3d660e7ae97bb8d3
Reddy-Somith <reddysomith.r@thoughtworks.com>
2022-11-08 13:16:13 +0530

    np 5

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
-resource "google_sql_database_instance" "main" {
-name = "instance_name"
+resource "google_sql_database_instance" "mysql" {
+name = "mysql-instance"
 database_version = "MYSQL_8_0"
 region = "us-east4"
 settings {
@@ -9,13 +9,13 @@ tier = "db-f1-micro"
 }
 resource "google_sql_database" "database" {
 name = "pos"
-instance = "${google_sql_database_instance.main.name}"
+instance = "${google_sql_database_instance.mysql.name}"
 charset = "utf8"
 collation = "utf8_general_ci"
 }
 resource "google_sql_user" "users" {
 name = "root"
-instance = "${google_sql_database_instance.main.name}"
+instance = "${google_sql_database_instance.mysql.name}"
 host = "%"
 password = "Sup3r$ecretP@ss"
 }
