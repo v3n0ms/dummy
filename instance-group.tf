c609e97db3b08587a9aabcce5552e67fdd545e37
Reddy-Somith <reddysomith.r@thoughtworks.com>
2022-11-10 12:03:09 +0530

    L 1

---
+++ instance-group.tf
@@ -0,0 +1,35 @@
+# resource "google_compute_health_check" "autohealing" {
+#   name                = "autohealing-health-check"
+#   check_interval_sec  = 5
+#   timeout_sec         = 5
+#   healthy_threshold   = 2
+#   unhealthy_threshold = 10 # 50 seconds
+
+#   http_health_check {
+#     request_path = "/healthz"
+#     port         = "8080"
+#   }
+# }
+
+resource "google_compute_instance_group_manager" "appserver" {
+  name = "pos-group"
+
+  base_instance_name = "app"
+  zone               = "us-west2-a"
+
+  version {
+    instance_template  = google_compute_instance_template.appserver.id
+  }
+
+
+
+  target_pools = [google_compute_target_pool.appserver.id]
+  target_size  = 1
+
+
+
+#   auto_healing_policies {
+#     health_check      = google_compute_health_check.autohealing.id
+#     initial_delay_sec = 300
+#   }
+}
--- instance-template.tf
+++ instance-template.tf
@@ -5,7 +5,7 @@ resource "google_compute_instance_template" "template" {
   region      = "us-west2"
 
   disk {
-    source_image = "debian-cloud/debian-11"
+    source_image = "cos-101-17162-40-16"
   }
 
   network_interface {
