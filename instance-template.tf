de3266d529c5f5e6cd83db77c05b87a6b29234c3
Reddy-Somith <reddysomith.r@thoughtworks.com>
2022-11-11 12:53:34 +0530

    idk 1

--- instance-template.tf
+++ instance-template.tf
@@ -36,9 +36,23 @@ resource "google_compute_instance_template" "template" {
 
   }
 
-  metadata_startup_script= "${file("script.sh")}"
+  metadata_startup_script = <<-EOF
+#!/bin/bash
 
-  tags = ["http-server"]
+toolbox \
+sudo apt-get install apt-transport-https ca-certificates gnupg \
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list \
+echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list \
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add - \
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - \
+sudo apt-get update && sudo apt-get install google-cloud-cli \
+
+gcloud auth activate-service-account --key-file=credentials.json \
+
+docker pull asia.gcr.io/dummy-project-365407/pos tag pos \
+docker run -p 8080:8080 -d -e DB_USER=root -e DB_NAME=pos -e DB_PASSWORD=Sup3r$ecretP@ss -e DB_HOST=34.150.222.6 pos 
+EOF
+  tags                    = ["http-server"]
 
   # secret default
   service_account {
