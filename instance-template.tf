4c515531d597c4241754eb19447dc533898cd046
Reddy-Somith <reddysomith.r@thoughtworks.com>
2022-11-11 17:09:06 +0530

    idk 10

--- instance-template.tf
+++ instance-template.tf
@@ -20,7 +20,7 @@ resource "google_compute_instance_template" "template" {
   region       = "us-west2"
 
   disk {
-    source_image = "debian-cloud/debian-11"
+    source_image = "ubuntu-os-cloud/ubuntu-2004-lts"
   }
 
   network_interface {
@@ -52,8 +52,7 @@ ACCESS_TOKEN=$(curl -H 'Metadata-Flavor: Google' $SVC_ACCT/token \
     | cut -d'"' -f 4)
 sudo docker login  -u _token -p $ACCESS_TOKEN https://asia.gcr.io
 
-sudo docker pull asia.gcr.io/dummy-project-365407/pos tag pos 
-sudo docker run -p 8080:8080 -d -e DB_USER=root -e DB_NAME=pos -e DB_PASSWORD=Sup3r$ecretP@ss -e DB_HOST=35.245.145.207 pos 
+sudo docker run -p 8080:8080 -d -e DB_USER=root -e DB_NAME=pos -e DB_PASSWORD=Sup3r$ecretP@ss -e DB_HOST=35.245.195.158 asia.gcr.io/dummy-project-365407/pos
 EOF
 
 
