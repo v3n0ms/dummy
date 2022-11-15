dcf2a0774ec9f2e801e9fb8296dd801befd0c0fb
Reddy-Somith <reddysomith.r@thoughtworks.com>
2022-11-15 12:52:51 +0530

    gg 6

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "google" {
 }
 
 resource "google_compute_global_address" "private_ip_address" {
-    provider="google-beta"
+    provider= google-beta
     name          = "${google_compute_network.default.name}"
     purpose       = "VPC_PEERING"
     address_type = "INTERNAL"
@@ -15,7 +15,7 @@ resource "google_compute_global_address" "private_ip_address" {
 }
 
 resource "google_service_networking_connection" "private_vpc_connection" {
-    provider="google-beta"
+    provider= google-beta
     network       = "${google_compute_network.default.self_link}"
     service       = "servicenetworking.googleapis.com"
     reserved_peering_ranges = ["${google_compute_global_address.private_ip_address.name}"]
