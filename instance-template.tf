45af37735c59e69eadd79e198c3fa55eb2bdc7bd
Reddy-Somith <reddysomith.r@thoughtworks.com>
2022-11-18 15:24:09 +0530

    gg 2

--- instance-template.tf
+++ instance-template.tf
@@ -68,7 +68,7 @@ ACCESS_TOKEN=$(curl -H 'Metadata-Flavor: Google' $SVC_ACCT/token \
     | cut -d'"' -f 4)
 sudo docker login  -u _token -p $ACCESS_TOKEN https://asia.gcr.io
 
-sudo docker run -p 8080:8080 -d -e DB_USER=${local.envs["USER"]} -e DB_NAME=${local.envs["NAME"]} -e DB_PASSWORD=${local.envs["PASSWORD"]} -e DB_HOST=${google_sql_database_instance.mysql.public_ip_address} asia.gcr.io/dummy-pro/pos
+sudo docker run -p 8080:8080 -d -e DB_USER=${local.envs["USER"]} -e DB_NAME=${local.envs["NAME"]} -e DB_PASSWORD=${local.envs["PASSWORD"]} -e DB_HOST=${google_sql_database_instance.mysql.public_ip_address} asia.gcr.io/dummy-project-365407/pos
 EOF
 
 
